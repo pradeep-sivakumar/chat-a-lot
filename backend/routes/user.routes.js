93d1a4667e5f2e8a0debe722fe2aaa18b60a5147
Pradeep Sivakumar <104561486+pradeep-sivakumar@users.noreply.github.com>
2024-02-10 19:45:07 +0530

    Get users

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,5 +1,6 @@
 import express from "express";
 import protectRoute from "../middlewares/protectRoute.js";
+import { getUsers } from "../controllers/user.controller.js";
 
 const router = express.Router();
 
