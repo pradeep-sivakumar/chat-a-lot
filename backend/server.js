293d62a4b01aee4a5937edcdca6c05929670512a
Pradeep Sivakumar <104561486+pradeep-sivakumar@users.noreply.github.com>
2024-02-11 00:14:08 +0530

    Socket IO

--- backend/server.js
+++ backend/server.js
@@ -7,19 +7,19 @@ import messageRoutes from './routes/message.routes.js';
 import userRoutes from './routes/user.routes.js';
 
 import connectDB from "./db/connectDB.js";
+import { app, server } from "./socket/socket.js";
 
-const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
 dotenv.config();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 
 app.use('/api/auth', authRoutes);
 app.use('/api/messages', messageRoutes);
 app.use('/api/users', userRoutes);
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     connectDB();
     console.log(`Server is running in ${PORT}`);
 });
