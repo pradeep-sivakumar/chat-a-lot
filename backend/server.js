458f841b5602ac3514a9344c87de4abd1e1eed55
Pradeep Sivakumar <104561486+pradeep-sivakumar@users.noreply.github.com>
2024-02-11 00:27:35 +0530

    Deployment

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from 'dotenv';
 import cookieParser from "cookie-parser";
+import path from 'path';
 
 import authRoutes from './routes/auth.routes.js';
 import messageRoutes from './routes/message.routes.js';
@@ -14,10 +15,17 @@ app.use(cookieParser());
 
 dotenv.config();
 const PORT = process.env.PORT || 8000;
+const __dirname = path.resolve();
 
 app.use('/api/auth', authRoutes);
 app.use('/api/messages', messageRoutes);
 app.use('/api/users', userRoutes);
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 server.listen(PORT, () => {
     connectDB();
