82c42a5a25407d70bf2aff373575f99821ad90c4
Pradeep Sivakumar <104561486+pradeep-sivakumar@users.noreply.github.com>
2024-02-12 22:34:47 +0530

    bug fix ui

--- frontend/src/components/messages/Messages.jsx
+++ frontend/src/components/messages/Messages.jsx
@@ -16,7 +16,7 @@ const Messages = () => {
 	}, [messages]);
 
 	return (
-		<div className='px-4 flex-1 overflow-auto'>
+		<div className='px-4 flex-1 overflow-auto mt-16 sm:mt-0'>
 			{!loading &&
 				messages.length > 0 &&
 				messages.map((message) => (
@@ -27,7 +27,7 @@ const Messages = () => {
 
 			{loading && [...Array(3)].map((_, idx) => <MessageSkeleton key={idx} />)}
 			{!loading && messages.length === 0 && (
-				<p className='text-center mt-16 sm:mt-0'>Send a message to start the conversation</p>
+				<p className='text-center'>Send a message to start the conversation</p>
 			)}
 		</div>
 	);
